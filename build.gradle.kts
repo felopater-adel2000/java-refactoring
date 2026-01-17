ea2e59766328133890248f851705c63af9639fa4
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-17 14:33:15 +0200

    Init Project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.refactoring.java"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("junit:junit:4.13.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/refactoring/java/MainTest.java
@@ -0,0 +1,14 @@
+package com.refactoring.java;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void textExample() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+
+}
