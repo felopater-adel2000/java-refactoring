334fc418f9f19b2eb27de277c5a08ee81bc96d1d
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-20 03:00:33 +0200

    add InvoiceData

---
+++ src/main/java/com/refactoring/java/split_phase_refactoring/finish/InvoiceData.java
@@ -0,0 +1,30 @@
+package com.refactoring.java.split_phase_refactoring.finish;
+
+public class InvoiceData {
+    private int totalAmount = 0;
+    private int volumeCredits = 0;
+
+    private String customerName = "";
+
+    public InvoiceData(int totalAmount, int volumeCredits, String customerName) {
+        this.totalAmount = totalAmount;
+        this.volumeCredits = volumeCredits;
+        this.customerName = customerName;
+    }
+
+    public int getVolumeCredits() {
+        return volumeCredits;
+    }
+
+    public void setVolumeCredits(int volumeCredits) {
+        this.volumeCredits = volumeCredits;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
