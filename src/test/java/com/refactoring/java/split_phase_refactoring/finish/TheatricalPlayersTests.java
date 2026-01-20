d4068f3ab4e65f656b32efd9bd9f5d7ad510e2e5
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-20 02:37:23 +0200

    add initial tests for TheatricalPlayers

---
+++ src/test/java/com/refactoring/java/split_phase_refactoring/finish/TheatricalPlayersTests.java
@@ -0,0 +1,30 @@
+package com.refactoring.java.split_phase_refactoring.finish;
+
+import com.refactoring.java.split_phase_refactoring.Invoice;
+import com.refactoring.java.split_phase_refactoring.Performance;
+import com.refactoring.java.split_phase_refactoring.Play;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.approvaltests.Approvals.verify;
+
+@UseReporter(JunitReporter.class)
+public class TheatricalPlayersTests {
+    @Test
+    public void exampleStatement() {
+        Invoice invoice = new Invoice("BigCo", List.of(
+                new Performance(new Play("Hamlet", "tragedy"), 55),
+                new Performance(new Play("As You Like It", "comedy"), 35),
+                new Performance(new Play("Othello", "tragedy"), 40)));
+
+        TheatricalPlayers theatricalPlayers = new TheatricalPlayers();
+        var result = theatricalPlayers.print(invoice);
+
+        System.out.println("Result = " + result);
+
+        verify(result);
+    }
+}
